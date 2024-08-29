2bcd94dbae0273a4620ee86260a3de7a8554e8c6
Rafael de Oliveira Gasparotto <81602919+rafaelgasparotto77@users.noreply.github.com>
2024-08-29 18:56:25 +0000

    2.5

--- Projeto/model.py
+++
@@ -1,5 +0,0 @@
-# Salve seu modelo
-import joblib
-
-# Supondo que `model` seja seu modelo treinado
-joblib.dump(, 'modelo_recomendacao.pkl')
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+import joblib
+import pandas as pd
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+
+app = Flask(__name__)
+
+# Carregue o modelo e o tokenizer
+model = joblib.load('modelo_recomendacao.pkl')
+tokenizer = joblib.load('tokenizer.pkl')  # Carregue o tokenizer também
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        review = request.form['review']
+        # Pré-processar a crítica
+        review_seq = tokenizer.texts_to_sequences([review])
+        padded_review = pad_sequences(review_seq, maxlen=100)
+        
+        # Faça a previsão
+        prediction = model.predict(padded_review)
+        sentiment = 'Positivo' if prediction[0][0] > 0.5 else 'Negativo'
+        
+        return render_template('index.html', prediction=sentiment)
+
+if __name__ == '__main__':
+    app.run(debug=True)
