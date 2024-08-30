57d6cc3ee5c4efe7c7996f6d55ade99b97065273
Rafael de Oliveira Gasparotto <81602919+rafaelgasparotto77@users.noreply.github.com>
2024-08-30 17:43:03 +0000

    2.7

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
-from flask import Flask, request, render_template
-import joblib
 import pandas as pd
-from keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokenizer
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+import numpy as np
 
 app = Flask(__name__)
 
-# Carregue o modelo e o tokenizer
-model = joblib.load('modelo_recomendacao.pkl')
-tokenizer = joblib.load('tokenizer.pkl')  # Carregue o tokenizer também
+# Carregue os dados do arquivo data.csv
+df = pd.read_csv('data.csv')
+
+# Carregue o modelo treinado
+model = load_model('modelo_recomendacao.h5')
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    genres = df['Genre'].unique()  # Obtenha os gêneros únicos
+    return render_template('index.html', genres=genres)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        review = request.form['review']
-        # Pré-processar a crítica
-        review_seq = tokenizer.texts_to_sequences([review])
-        padded_review = pad_sequences(review_seq, maxlen=100)
-        
-        # Faça a previsão
-        prediction = model.predict(padded_review)
-        sentiment = 'Positivo' if prediction[0][0] > 0.5 else 'Negativo'
-        
-        return render_template('index.html', prediction=sentiment)
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    selected_genre = request.form['genre']
+    # Filtre os filmes pelo gênero selecionado
+    filtered_movies = df[df['Genre'] == selected_genre]
+
+    # Prepare os dados para a previsão
+    X_predict = filtered_movies[['Movie Rating', 'Votes']].values
+    predicted_ratings = model.predict(X_predict)
+
+    # Adicione as previsões ao DataFrame
+    filtered_movies['Predicted Rating'] = predicted_ratings
+
+    # Obtenha os 5 filmes com as melhores previsões
+    top_movies = filtered_movies.nlargest(5, 'Predicted Rating')
+
+    recommendations = top_movies[['Movie Name', 'Year of Release', 'Director', 'Genre', 'Description']].to_dict(orient='records')
+    return render_template('recommendations.html', movies=recommendations)
 
 if __name__ == '__main__':
     app.run(debug=True)
